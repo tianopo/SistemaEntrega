55d589cd9f4f5f886eddb7be572f3c351a4801be
Matheus Henrique de Abreu <matheuslink18@hotmail.com>
2023-11-17 22:10:55 -0300

    first commit

---
+++ src/main/java/com/senac/entregas/Entregas.java
@@ -0,0 +1,12 @@
+package com.senac.entregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entregas {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Entregas.class, args);
+    }
+}
---
+++ src/main/java/com/senac/entregas/controller/EntregasController.java
@@ -0,0 +1,20 @@
+package com.senac.entregas.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EntregasController {
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("pagina", "pedidos");
+        return "index";
+    }
+
+    @GetMapping("/motoboy")
+    public String motoboy(Model model){
+        model.addAttribute("pagina", "motoboy");
+        return "index";
+    }
+}
